58350c4241ca4ee9ba6941cf5bd80fda6c235842
gIsForGravity <37271917+gIsForGravity@users.noreply.github.com>
2022-02-28 18:28:56 -0500

    Add physx.c and physx.h

---
+++ src/physx.c
@@ -0,0 +1,9 @@
+/**
+ * Filename: physx.c
+ * 
+ * Description: physics yay
+ * 
+ * Copyright (c) Gavin Tantleff 2022. All Rights Reserved.
+ */
+
+#include "physx.h"
---
+++ src/physx.h
@@ -0,0 +1,27 @@
+/**
+ * Filename: physx.h
+ * 
+ * Description: physics yay
+ * 
+ * Copyright (c) Gavin Tantleff 2022. All Rights Reserved.
+ */
+
+#ifndef PHYSX_H
+#define PHYSX_H
+
+/* Includes */
+#include "types/basetypes.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* PHYSX_H */
